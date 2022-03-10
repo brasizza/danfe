c26215e224e3cc1960db60beb702c4006adf88d4
Marcus Brasizza <marcusbrasizza@macbook-marcus.local>
2022-03-10 01:43:22 -0300

    danfe

--- lib/danfe_printer.dart
+++ lib/danfe_printer.dart
@@ -24,22 +24,23 @@ class DanfePrinter {
     // Print image
     bytes += generator.rawBytes([27, 97, 49]);
 
-    bytes += generator.text(danfe?.dados?.emit?.xFant ?? '', styles: const PosStyles(align: PosAlign.center));
+    bytes += generator.text(danfe?.dados?.emit?.xFant ?? '',
+        styles: const PosStyles(
+          align: PosAlign.center,
+          fontType: PosFontType.fontB,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ));
     bytes += generator.text((danfe?.dados?.emit?.cNPJ ?? ''), styles: const PosStyles(align: PosAlign.center));
     bytes += generator.text((danfe?.dados?.emit?.enderEmit?.xLgr ?? '') + ', ' + ((danfe?.dados?.emit?.enderEmit?.nro ?? '')), styles: const PosStyles(align: PosAlign.center));
     bytes += generator.hr();
     if ((danfe?.tipo ?? 'CFe') == 'CFe') {
-      bytes += generator.text('Nota Fiscal Eletronica - SAT ',
-          styles: const PosStyles(
-            align: PosAlign.center,
-            height: PosTextSize.size2,
-            width: PosTextSize.size2,
-          ),
-          linesAfter: 1);
+      bytes += generator.text('Nota Fiscal Eletronica - SAT ', styles: const PosStyles(align: PosAlign.center, bold: true, height: PosTextSize.size1, width: PosTextSize.size1), linesAfter: 1);
     } else {
       bytes += generator.text('Nota Fiscal Eletronica - NFC-E ',
           styles: const PosStyles(
             align: PosAlign.center,
+            bold: true,
             height: PosTextSize.size1,
             width: PosTextSize.size1,
           ),
@@ -52,7 +53,7 @@ class DanfePrinter {
     bytes += generator.feed(1);
     bytes += generator.row([
       PosColumn(text: 'DESCRICAO', width: 5),
-      PosColumn(text: 'QTD', width: 1),
+      PosColumn(text: 'QTD', width: 1, styles: const PosStyles(align: PosAlign.right)),
       PosColumn(text: 'VLUN', width: 3, styles: const PosStyles(align: PosAlign.right)),
       PosColumn(text: 'VLTOT', width: 3, styles: const PosStyles(align: PosAlign.right)),
     ]);
@@ -60,9 +61,9 @@ class DanfePrinter {
       for (Det det in danfe!.dados!.det!) {
         bytes += generator.row([
           PosColumn(text: det.prod?.xProd ?? '', width: 5),
-          PosColumn(text: formatNumber(det.prod?.qCom ?? ''), width: 1),
-          PosColumn(text: formatMoneyMilhar(det.prod?.vUnCom ?? '', modeda: 'pt_BR', simbolo: r'R$'), width: 3),
-          PosColumn(text: formatMoneyMilhar(det.prod?.vProd ?? '', modeda: 'pt_BR', simbolo: r'R$'), width: 3),
+          PosColumn(text: formatNumber(det.prod?.qCom ?? '') + 'x', width: 1, styles: const PosStyles(align: PosAlign.right)),
+          PosColumn(text: formatMoneyMilhar(det.prod?.vUnCom ?? '', modeda: 'pt_BR', simbolo: r'R$'), width: 3, styles: const PosStyles(align: PosAlign.right)),
+          PosColumn(text: formatMoneyMilhar(det.prod?.vProd ?? '', modeda: 'pt_BR', simbolo: r'R$'), width: 3, styles: const PosStyles(align: PosAlign.right)),
         ]);
       }
     }
@@ -86,21 +87,26 @@ class DanfePrinter {
             width: PosTextSize.size1,
           )),
     ]);
+    bytes += generator.hr();
 
     bytes += generator.rawBytes([27, 97, 49]);
     bytes += generator.text('CHAVE DE ACESSO DA NOTA FISCAL ELETRONICA', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text(danfe?.dados?.chaveNota ?? '', styles: const PosStyles(align: PosAlign.center));
+    bytes += generator.feed(1);
 
-    bytes += generator.rawBytes([27, 97, 48]);
+    bytes += generator.text(danfe?.dados?.chaveNota ?? '', styles: const PosStyles(align: PosAlign.center, bold: true));
+    bytes += generator.feed(1);
+
     DateTime data = DateTime.now();
 
     var outputFormat = DateFormat('dd/MM/yyyy HH:mm:ss');
     String dataEmissao = outputFormat.format(data);
     bytes += generator.text('Emitida em: ' + dataEmissao, styles: const PosStyles(align: PosAlign.center));
     bytes += generator.feed(1);
+    bytes += generator.rawBytes([27, 97, 49]);
     bytes += generator.qrcode(danfe?.qrcodePrinter ?? '', size: QRSize.Size5, cor: QRCorrection.M);
     bytes += generator.feed(1);
     bytes += generator.cut();
+    bytes += generator.reset();
 
     return bytes;
   }
