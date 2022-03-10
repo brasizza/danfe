2566e951503782693fb9e4f912961ed1ba66cd02
Marcus Brasizza <marcusbrasizza@macbook-marcus.local>
2022-03-10 02:27:59 -0300

    danfe

--- lib/src/models/sat/danfe.dart
+++ lib/src/models/sat/danfe.dart
@@ -461,8 +461,8 @@ class Total {
   factory Total.fromMap(Map<String, dynamic> map) {
     return Total(
       valorTotal: (map.containsKey('vCFe') ? map['vCFe'] : map['ICMSTot']['vNF']),
-      desconto: (map.containsKey('vCFe') ? map['DescAcrEntr']['vDescSubtot'] : map['ICMSTot']['vDesc']),
-      acrescimo: (map.containsKey('vCFe') ? '0' : map['ICMSTot']['vOutro']),
+      desconto: (map.containsKey('vCFe') ? (map['DescAcrEntr']?['vDescSubtot']) : map['ICMSTot']['vDesc']),
+      acrescimo: (map.containsKey('vCFe') ? null : map['ICMSTot']['vOutro']),
     );
   }
 
