3ffa81c347c123049fc1a758afb180eda4fde0ce
Marcus Brasizza <marcusbrasizza@macbook-marcus.local>
2022-03-10 00:40:39 -0300

    final

--- lib/danfe_printer.dart
+++ lib/danfe_printer.dart
@@ -9,52 +9,51 @@ class DanfePrinter {
     final generator = Generator(sizePaper, profile);
     List<int> bytes = [];
     // Print image
+    bytes += generator.rawBytes([27, 97, 49]);
 
-    bytes += generator.text('GROCERYLY',
-        styles: const PosStyles(
-          align: PosAlign.center,
-          height: PosTextSize.size2,
-          width: PosTextSize.size2,
-        ),
-        linesAfter: 1);
-
-    bytes += generator.text('889  Watson Lane', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text('New Braunfels, TX', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text('Tel: 830-221-1234', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text('Web: www.example.com', styles: const PosStyles(align: PosAlign.center), linesAfter: 1);
-
+    bytes += generator.text(danfe?.dados?.emit?.xFant ?? '', styles: const PosStyles(align: PosAlign.center));
+    bytes += generator.text((danfe?.dados?.emit?.cNPJ ?? ''), styles: const PosStyles(align: PosAlign.center));
+    bytes += generator.text((danfe?.dados?.emit?.enderEmit?.xLgr ?? '') + ', ' + ((danfe?.dados?.emit?.enderEmit?.nro ?? '')), styles: const PosStyles(align: PosAlign.center));
     bytes += generator.hr();
-    bytes += generator.row([
-      PosColumn(text: 'Qty', width: 1),
-      PosColumn(text: 'Item', width: 7),
-      PosColumn(text: 'Price', width: 2, styles: const PosStyles(align: PosAlign.right)),
-      PosColumn(text: 'Total', width: 2, styles: const PosStyles(align: PosAlign.right)),
-    ]);
+    if ((danfe?.tipo ?? 'CFe') == 'CFe') {
+      bytes += generator.text('Nota Fiscal Eletronica - SAT ',
+          styles: const PosStyles(
+            align: PosAlign.center,
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          ),
+          linesAfter: 1);
+    } else {
+      bytes += generator.text('Nota Fiscal Eletronica - NFC-E ',
+          styles: const PosStyles(
+            align: PosAlign.center,
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          ),
+          linesAfter: 1);
+    }
+
+    bytes += generator.rawBytes([27, 97, 49]);
+    bytes += generator.text("CPF/CNPJ do consumidor: " + (danfe?.dados?.dest?.cpf ?? ''), styles: const PosStyles(align: PosAlign.left));
+    bytes += generator.text("Nota : " + (danfe?.dados?.ide?.nNF ?? ''), styles: const PosStyles(align: PosAlign.left));
 
     bytes += generator.row([
-      PosColumn(text: '2', width: 1),
-      PosColumn(text: 'ONION RINGS', width: 7),
-      PosColumn(text: '0.99', width: 2, styles: const PosStyles(align: PosAlign.right)),
-      PosColumn(text: '1.98', width: 2, styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(text: 'DESCRICAO', width: 7),
+      PosColumn(text: 'QTD', width: 1),
+      PosColumn(text: 'VLUN', width: 2, styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(text: 'VLTOT', width: 2, styles: const PosStyles(align: PosAlign.right)),
     ]);
-    bytes += generator.row([
-      PosColumn(text: '1', width: 1),
-      PosColumn(text: 'PIZZA', width: 7),
-      PosColumn(text: '3.45', width: 2, styles: const PosStyles(align: PosAlign.right)),
-      PosColumn(text: '3.45', width: 2, styles: const PosStyles(align: PosAlign.right)),
-    ]);
-    bytes += generator.row([
-      PosColumn(text: '1', width: 1),
-      PosColumn(text: 'SPRING ROLLS', width: 7),
-      PosColumn(text: '2.99', width: 2, styles: const PosStyles(align: PosAlign.right)),
-      PosColumn(text: '2.99', width: 2, styles: const PosStyles(align: PosAlign.right)),
-    ]);
-    bytes += generator.row([
-      PosColumn(text: '3', width: 1),
-      PosColumn(text: 'CRUNCHY STICKS', width: 7),
-      PosColumn(text: '0.85', width: 2, styles: const PosStyles(align: PosAlign.right)),
-      PosColumn(text: '2.55', width: 2, styles: const PosStyles(align: PosAlign.right)),
-    ]);
+    if (danfe?.dados?.det != null) {
+      for (Det det in danfe!.dados!.det!) {
+        bytes += generator.row([
+          PosColumn(text: det.prod?.xProd ?? '', width: 7),
+          PosColumn(text: det.prod?.qCom ?? '', width: 1),
+          PosColumn(text: det.prod?.vUnCom ?? '', width: 2, styles: const PosStyles(align: PosAlign.right)),
+          PosColumn(text: det.prod?.vProd ?? '', width: 2, styles: const PosStyles(align: PosAlign.right)),
+        ]);
+      }
+    }
+
     bytes += generator.hr();
 
     bytes += generator.row([
@@ -66,7 +65,7 @@ class DanfePrinter {
             width: PosTextSize.size2,
           )),
       PosColumn(
-          text: '\$10.97',
+          text: danfe?.dados?.total?.valorTotal ?? '',
           width: 6,
           styles: const PosStyles(
             align: PosAlign.right,
@@ -77,19 +76,19 @@ class DanfePrinter {
 
     bytes += generator.hr(ch: '=', linesAfter: 1);
 
-    bytes += generator.row([
-      PosColumn(text: 'Cash', width: 8, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-      PosColumn(text: '\$15.00', width: 4, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-    ]);
-    bytes += generator.row([
-      PosColumn(text: 'Change', width: 8, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-      PosColumn(text: '\$4.03', width: 4, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-    ]);
+    // bytes += generator.row([
+    //   PosColumn(text: 'Cash', width: 8, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    //   PosColumn(text: '\$15.00', width: 4, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    // ]);
+    // bytes += generator.row([
+    //   PosColumn(text: 'Change', width: 8, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    //   PosColumn(text: '\$4.03', width: 4, styles: const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    // ]);
 
-    bytes += generator.feed(2);
-    bytes += generator.text('Thank you!', styles: const PosStyles(align: PosAlign.center, bold: true));
-    final now = DateTime.now();
-    bytes += generator.text(now.toIso8601String(), styles: const PosStyles(align: PosAlign.center), linesAfter: 2);
+    // bytes += generator.feed(2);
+    // bytes += generator.text('Thank you!', styles: const PosStyles(align: PosAlign.center, bold: true));
+    // final now = DateTime.now();
+    // bytes += generator.text(now.toIso8601String(), styles: const PosStyles(align: PosAlign.center), linesAfter: 2);
     bytes += generator.qrcode('example.com');
     bytes += generator.feed(1);
     bytes += generator.cut();
--- lib/src/models/sat/danfe.dart
+++ lib/src/models/sat/danfe.dart
@@ -241,6 +241,58 @@ class Ide {
   factory Ide.fromJson(String source) => Ide.fromMap(json.decode(source));
 }
 
+class EnderEmit {
+  String? xLgr;
+  String? nro;
+  String? xCpl;
+  String? xBairro;
+  String? cMun;
+  String? xMun;
+  String? uF;
+  String? cEP;
+  String? cPais;
+  String? xPais;
+  String? fone;
+
+  EnderEmit({this.xLgr, this.nro, this.xCpl, this.xBairro, this.cMun, this.xMun, this.uF, this.cEP, this.cPais, this.xPais, this.fone});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'xLgr': xLgr,
+      'nro': nro,
+      'xCpl': xCpl,
+      'xBairro': xBairro,
+      'cMun': cMun,
+      'xMun': xMun,
+      'uF': uF,
+      'cEP': cEP,
+      'cPais': cPais,
+      'xPais': xPais,
+      'fone': fone,
+    };
+  }
+
+  factory EnderEmit.fromMap(Map<String, dynamic> map) {
+    return EnderEmit(
+      xLgr: map['xLgr'],
+      nro: map['nro'],
+      xCpl: map['xCpl'],
+      xBairro: map['xBairro'],
+      cMun: map['cMun'],
+      xMun: map['xMun'],
+      uF: map['UF'],
+      cEP: map['CEP'],
+      cPais: map['cPais'],
+      xPais: map['xPais'],
+      fone: map['fone'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory EnderEmit.fromJson(String source) => EnderEmit.fromMap(json.decode(source));
+}
+
 class Emit {
   String? cNPJ;
   String? xNome;
@@ -249,15 +301,8 @@ class Emit {
   String? iM;
   String? cRegTrib;
   String? indRatISSQN;
-  Emit({
-    this.cNPJ,
-    this.xNome,
-    this.xFant,
-    this.iE,
-    this.iM,
-    this.cRegTrib,
-    this.indRatISSQN,
-  });
+  EnderEmit? enderEmit;
+  Emit({this.cNPJ, this.xNome, this.xFant, this.iE, this.iM, this.cRegTrib, this.indRatISSQN, this.enderEmit});
 
   Map<String, dynamic> toMap() {
     return {
@@ -268,6 +313,7 @@ class Emit {
       'iM': iM,
       'cRegTrib': cRegTrib,
       'indRatISSQN': indRatISSQN,
+      'enderEmit': enderEmit?.toMap(),
     };
   }
 
@@ -280,6 +326,7 @@ class Emit {
       iM: map['IM'],
       cRegTrib: map['cRegTrib'],
       indRatISSQN: map['indRatISSQN'],
+      enderEmit: map['enderEmit'] != null ? EnderEmit.fromMap(map['enderEmit']) : null,
     );
   }
 
@@ -289,20 +336,20 @@ class Emit {
 }
 
 class Dest {
-  String? cPF;
+  String? cpf;
   String? xNome;
-  Dest({this.cPF, this.xNome});
+  Dest({this.cpf, this.xNome});
 
   Map<String, dynamic> toMap() {
     return {
-      'cPF': cPF,
+      'cpf': cpf,
       'xNome': xNome,
     };
   }
 
   factory Dest.fromMap(Map<String, dynamic> map) {
     return Dest(
-      cPF: map['CPF'],
+      cpf: map['CPF'],
       xNome: map['xNome'],
     );
   }
